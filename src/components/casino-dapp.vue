4e716f469dc399d83af5258f39bb7be35be6c843
David Porras <david.porras12@gmail.com>
2018-05-23 06:42:41 -0400

    Initial commit

---
+++ src/components/casino-dapp.vue
@@ -0,0 +1,13 @@
+<template>
+  <hello-metamask></hello-metamask>
+</template>
+
+<script>
+import HelloMetamask from './hello-metamask'
+export default {
+  name: 'casino-dapp',
+  components: {
+    'hello-metamask': HelloMetamask
+  }
+}
+</script>
---
+++ src/util/constants/networks.js
@@ -0,0 +1,9 @@
+export const NETWORKS = {
+  '1': 'Mainnet',
+  '2': '(Deprecated) Morden Testnet',
+  '3': 'Ropsten Testnet',
+  '4': 'Rinkeby Testnet',
+  '42': 'Kovan Testnet',
+  '4447': 'Truffle Develop Network',
+  '5777': 'Ganache Blockchain'
+}
